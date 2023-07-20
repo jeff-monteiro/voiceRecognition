14cb81ea8bfd20ec23c6512b272ee0947fbaf014
Jefferson <jeffmonteiro61@icloud.com>
2023-07-19 23:40:37 -0300

    feat:validation_number_equals_numberGenerated

--- app/validation.js
+++ app/validation.js
@@ -9,6 +9,13 @@ function verifyAttemptIsValid(attempt){
     elementAttempt.innerHTML += `<div>Valor inválido o valor precisa estar entre ${minorValue} e ${majorValue}</div>`
   }
 
+  if(number === numberGenerator){
+    document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numberGenerator}</h3>
+    `
+  }
+
 }
 
 function invalidAttempt(number) {
