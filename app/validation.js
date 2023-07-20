73e951bcf01eaefe113a53d2eccaed6d570f5e1e
Jefferson <jeffmonteiro61@icloud.com>
2023-07-19 23:30:33 -0300

    feat:showing_on_screen

--- app/validation.js
+++ app/validation.js
@@ -2,10 +2,19 @@ function verifyAttemptIsValid(attempt){
   const number = +attempt
 
   if(invalidAttempt(number)){
-    console.log("Valor inválido")
+    elementAttempt.innerHTML += '<div>Valor inválido</div>'
   }
+
+  if(numberBiggerThanRequested(number)){
+    elementAttempt.innerHTML += `<div>Valor inválido o valor precisa estar entre ${minorValue} e ${majorValue}</div>`
+  }
+
 }
 
 function invalidAttempt(number) {
     return Number.isNaN(number)
+}
+
+function numberBiggerThanRequested(number){
+    return number > majorValue || number < minorValue
 }
